1f89f1cfd230f50439285783226f2c39e901734f
martinviszlai <martin.viszlai@gmail.com>
2014-04-25 15:52:34 +0200

    wer

---
+++ TSWP/src/com/mv2studio/tswp/communication/URLs.java
@@ -0,0 +1,7 @@
+package com.mv2studio.tswp.communication;
+
+public class URLs {
+
+	public static final String fileUpload = "http://tswp.martinviszlai.com/upload.php";
+	public static final String fileDownload = "http://tswp.martinviszlai.com/uploadw.php";
+}
